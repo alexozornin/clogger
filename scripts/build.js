341ed984f2c90ea9388c1663c7ff8611bea6e320
Alexander <alexozornin97@gmail.com>
2019-05-27 15:48:09 +0500

    v 1.0.3 manual build option

--- scripts/build.js
+++ scripts/build.js
@@ -16,16 +16,22 @@ const platform = os.platform();
 
 const script = scripts[platform];
 
-switch (script) {
-    case 'gcc':
-        console.log(`Building for platform ${platform}...`);
-        child.spawn(path.join(__dirname, 'gccbuild.sh'));
-        break;
-    case 'vc':
-        console.log(`Building for platform ${platform}...`);
-        child.spawn(path.join(__dirname, 'vcbuild.bat'));
-        break;
-    default:
-        console.log(`Platform ${platform} is not supported`);
-        break;
+try {
+    switch (script) {
+        case 'gcc':
+            console.log(`Building for platform ${platform}...`);
+            child.spawn(path.join(__dirname, 'gccbuild.sh'));
+            break;
+        case 'vc':
+            console.log(`Building for platform ${platform}...`);
+            child.spawn(path.join(__dirname, 'vcbuild.bat'));
+            break;
+        default:
+            console.log(`Platform ${platform} is not supported`);
+            break;
+    }
+}
+catch (err) {
+    console.log('Cannot build clogger. You might be able to build manually.');
+    console.log(err);
 }
